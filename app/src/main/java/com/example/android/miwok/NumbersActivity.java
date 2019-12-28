e50baa9cfa2df4775bf33f19dba34b45ba849674
goelkiran <59161047+goelkiran@users.noreply.github.com>
2019-12-28 18:21:48 +0530

    Tried gridView with 3 columns and redundant number strings Surprisingly ArrayList can hold duplicate objects (Strings in my case)

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -2,6 +2,7 @@ package com.example.android.miwok;
 
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
+import android.widget.GridView;
 import android.widget.ListView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -29,9 +30,41 @@ public class NumbersActivity extends AppCompatActivity {
         numberStrings.add("nine");
         numberStrings.add("ten");
 
+
+        numberStrings.add("one");
+        numberStrings.add("two");
+        numberStrings.add("three");
+        numberStrings.add("four");
+        numberStrings.add("five");
+        numberStrings.add("six");
+        numberStrings.add("seven");
+        numberStrings.add("eight");
+        numberStrings.add("nine");
+        numberStrings.add("ten");
+        numberStrings.add("one");
+        numberStrings.add("two");
+        numberStrings.add("three");
+        numberStrings.add("four");
+        numberStrings.add("five");
+        numberStrings.add("six");
+        numberStrings.add("seven");
+        numberStrings.add("eight");
+        numberStrings.add("nine");
+        numberStrings.add("ten");
+        numberStrings.add("one");
+        numberStrings.add("two");
+        numberStrings.add("three");
+        numberStrings.add("four");
+        numberStrings.add("five");
+        numberStrings.add("six");
+        numberStrings.add("seven");
+        numberStrings.add("eight");
+        numberStrings.add("nine");
+        numberStrings.add("ten");
+
         ArrayAdapter<String> itemsAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, numberStrings);
 
-        ListView listView = (ListView) findViewById(R.id.root_view_numbers);
+        GridView listView = (GridView) findViewById(R.id.root_view_numbers);
 
         listView.setAdapter(itemsAdapter);
 
